9e36728058e68332d08cf6594438453926be8b73
Mostafa Abokhadra <98665188+mostafa-abokhadra@users.noreply.github.com>
2025-07-29 22:07:19 +0300

    better mathematical way to solve palindrome problem

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -23,3 +23,17 @@ int main() {
         cout << isPalindrome(1234567899) << endl;
         return 0;
 }
+
+// better method
+
+bool isPalindrome(int n) {
+    int reverse = 0;
+  
+    int temp = abs(n);
+    while (temp != 0) {
+        reverse = (reverse * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+
+    return (reverse == abs(n));
+}
